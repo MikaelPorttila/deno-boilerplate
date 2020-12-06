9a6454e3a8f8c68aafb20ca7479dd3c6f8fae95a
Mikael Porttila <mikael.porttila@gmail.com>
2020-12-06 15:49:33 +0100

    Use top level await and comment changes.

--- dep.ts
+++ dep.ts
@@ -1,5 +1,5 @@
-// First party import
+// First-party import
 export * as log from "https://deno.land/std@0.79.0/log/mod.ts";
 
-// third party import
+// Third-party import
 export { Application } from "https://deno.land/x/oak/mod.ts";
--- dev.ts
+++ dev.ts
@@ -1,7 +1,3 @@
-import { Entry, getEntry } from "./mod.ts";
-
 import { startup } from "./src/web.ts";
 
-(async () => {
-  await startup();
-})();
+await startup();
